e1f23c756e63caf8ff1506e7f075858ae5fa021f
nbswords <dessertn626@gmail.com>
2021-09-12 07:42:19 +0800

    pop Ina

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var img = document.getElementById("popcat1");
+var img = document.getElementById("popIna1");
 var count = document.getElementById("score");
 var TWScore = document.getElementById('my_score');
 var score = 0;
